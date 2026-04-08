4e42c92a90b85b6741450307174d9023d5e45d79
Csaba44 <csabi16sheldon@gmail.com>
2026-04-08 15:49:51 +0200

    Fix: altitude in ft

--- XPlaneTracker/main.py
+++ XPlaneTracker/main.py
@@ -52,13 +52,13 @@ try:
         if lat is not None and lon is not None and alt is not None:
             lat = round(lat, 5)
             lon = round(lon, 5)
-            alt = int(alt)
+            alt = int(alt * 3.28084)
 
             if lat != last_lat or lon != last_lon or alt != last_alt:
                 current_time = round(time.time(), 2)
                 formatted_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
                 
-                print(f"[{formatted_time}] Lat: {lat}, Lon: {lon}, Alt: {alt}")
+                print(f"[{formatted_time}] Lat: {lat}, Lon: {lon}, Alt: {alt} ft")
                 
                 flight_path_data["path"].append([current_time, lat, lon, alt])
                 
